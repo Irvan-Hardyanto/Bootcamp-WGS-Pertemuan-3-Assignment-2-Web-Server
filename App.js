1cb7192aa42dd9f39c6643ed0f6237f62aa24f79
Irvan-Hardyanto <hardyanto.irvan@gmail.com>
2022-09-08 14:52:30 +0700

    Routing Sederhana + tambah halaman HTML

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 const http=require('http');
+const fs = require('fs');
 
 http.createServer((req,res)=>{
     const url = req.url;
@@ -8,14 +9,37 @@ http.createServer((req,res)=>{
         'Content-Type': 'text/html'
     })//kalo bagian ini di bawah... ya data datadi bawah ini akan diagram pre
     if(url=='/about'){
-        res.write('<h2>This is about page</h2>');//meskipun kita cuma kirim h1 h2 tanpa ada tag html, browser tetep tau kalo ini html
-        res.end();
+        fs.readFile('./about.html',(err,data)=>{
+            if(err){
+                res.writeHead(404);//buat status code
+                res.write('Error: page not found');
+            }else{
+                res.write(data);
+            }
+            res.end();
+        })
     }else if(url=='/contact'){
-        res.write('<h2>This is contact page</h2>');
-        res.end();
+        fs.readFile('./contact.html',(err,data)=>{
+            if(err){
+                res.writeHead(404);//buat status code
+                res.write('Error: page not found');
+            }else{
+                res.write(data);
+            }
+            res.end();
+        })
     }else{
-        res.write('hello worldddd');
-        res.end();
+        //iini method asinkronus... bisa pake promise...
+        fs.readFile('./index.html',(err,data)=>{
+            if(err){
+                res.writeHead(404);//buat status code
+                res.write('Error: page not found');
+            }else{            
+                res.write(data);
+            }
+            res.end();
+        })
+        //res.end();ini cukup sekali aja.. gak usah berkali-kali.
     }
 }).listen(3000,()=>{
     console.log('Server is running on port 3000');
